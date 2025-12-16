50c7542fe032a8bf6503b01cdad452eba97aa248
Nuri6336 <abut07946@gmail.com>
2025-12-16 08:38:43 +0600

    section 2 added: set up 3 projects with properguidence

---
+++ accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -0,0 +1,35 @@
+package com.eazybytes.accounts;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Accounts microservice rest api documentation",
+                description = "EasyBank Accounts microservice Rest API documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Taeb Nuri",
+                        email = "taeb@gmail.com",
+                        url = "https://www.easybytes.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.easybytes.com"
+                )
+        )
+)
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+}
